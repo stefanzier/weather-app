26f182f0c4feb5e7acbc634b11a2df09cc4f5c58
Stefan Zier <szier@icloud.com>
2016-12-25 16:24:44 -0800

    Log address, latitude, longitude

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1829+Los+Altos+Dr',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
