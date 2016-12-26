1a8c2a8368c86f7243c87de4681066665956272a
Stefan Zier <szier@icloud.com>
2016-12-25 16:28:41 -0800

    Installed Yargs and Set Up Configurations

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1829+Los+Altos+Dr',
