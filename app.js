d143200f16fab32d236c8b78cffe87d42f184e98
Stefan Zier <szier@icloud.com>
2016-12-25 16:33:54 -0800

    Search Any Address

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
+var encodedAddress = encodeURIComponent(argv.a)
+
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1829+Los+Altos+Dr',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
